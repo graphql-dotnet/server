e32a66414e0896124f5453dc4e7262a78c168fc1
Pekka Heikura <pekkah@gmail.com>
2017-11-15 15:28:18 +0200

    Use write queue (#29)
    * Use write queue
    
    * Dispose writer timer
    
    * Some fixes

--- src/WebSockets/Messages/WebSocketClient.cs
+++ src/WebSockets/Messages/WebSocketClient.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Concurrent;
 using System.IO;
 using System.Net.WebSockets;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using GraphQL.Server.Transports.WebSockets.Abstractions;
+using GraphQL.Server.Transports.WebSockets.Extensions;
 
 namespace GraphQL.Server.Transports.WebSockets.Messages
 {
@@ -12,21 +15,45 @@ namespace GraphQL.Server.Transports.WebSockets.Messages
     {
         private readonly WebSocket _socket;
 
+        private readonly ConcurrentQueue<ArraySegment<byte>> _writeQueue = new ConcurrentQueue<ArraySegment<byte>>();
+        private IDisposable _writer;
+
+        //todo(pekka): allow configuration
+        private static readonly TimeSpan WriteMessageInterval = TimeSpan.FromMilliseconds(10);
+
         public WebSocketClient(WebSocket socket)
         {
             _socket = socket;
+            _writer = Observable.Timer(TimeSpan.FromMilliseconds(0), WriteMessageInterval)
+                .SubscribeAsync(_ => InternalWriteMessage(), OnWriteError, () => { });
+        }
+
+        private Task InternalWriteMessage()
+        {
+            //todo: should throw or not?
+            if (_socket.CloseStatus.HasValue)
+                return Task.CompletedTask;
+
+            if (_writeQueue.TryDequeue(out var message))
+            {
+                return _socket.SendAsync(message, WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private void OnWriteError(Exception error)
+        {
+           
         }
 
         public WebSocketCloseStatus? CloseStatus => _socket.CloseStatus;
 
         public Task WriteMessageAsync(string message)
         {
-            //todo: should throw or not?
-            if (_socket.CloseStatus.HasValue)
-                return Task.CompletedTask;
-
             var messageSegment = new ArraySegment<byte>(Encoding.UTF8.GetBytes(message));
-            return _socket.SendAsync(messageSegment, WebSocketMessageType.Text, true, CancellationToken.None);
+            _writeQueue.Enqueue(messageSegment);
+            return Task.CompletedTask;
         }
 
         public async Task<string> ReadMessageAsync()
@@ -67,6 +94,8 @@ namespace GraphQL.Server.Transports.WebSockets.Messages
 
         public Task CloseAsync()
         {
+            _writer.Dispose();
+            
             if (_socket.State != WebSocketState.Open)
                 return Task.CompletedTask;
 
