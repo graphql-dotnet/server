fd36a56e7db5ac2efa069d17dc0f1828a85b71d9
Raul Hidalgo Caballero <raul.hidalgo.sopa@gmail.com>
2018-01-27 17:17:40 +0100

    Added cshtml and renderer

---
+++ src/Ui.Playground/Internal/PlaygroundPageModel.cs
@@ -0,0 +1,35 @@
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace GraphQL.Server.Ui.Playground.Internal {
+
+	// https://docs.microsoft.com/en-us/aspnet/core/mvc/razor-pages/?tabs=netcore-cli
+	internal class PlaygroundPageModel {
+
+		private static string playgroundCSHtml;
+
+		private readonly GraphQLPlaygroundOptions settings;
+
+		public PlaygroundPageModel(GraphQLPlaygroundOptions settings) {
+			this.settings = settings;
+		}
+
+		public string Render() {
+			if (playgroundCSHtml != null) {
+				return playgroundCSHtml;
+			}
+			var assembly = typeof(PlaygroundPageModel).GetTypeInfo().Assembly;
+            using (var manifestResourceStream = assembly.GetManifestResourceStream("GraphQL.Server.Ui.Playground.Internal.playground.cshtml")) {
+                using (var streamReader = new StreamReader(manifestResourceStream)) {
+                    var builder = new StringBuilder(streamReader.ReadToEnd());
+                    builder.Replace("@Model.GraphQLEndPoint", this.settings.GraphQLEndPoint);
+                    playgroundCSHtml = builder.ToString();
+                    return this.Render();
+                }
+            }
+		}
+
+	}
+
+}
