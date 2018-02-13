4683672bf852001c8b8a97a6784bad81fdb7c3ab
Søren Bjergmark <sahb1239@gmail.com>
2018-02-13 15:38:30 +0100

    Added extention method for registrering a IUserContextBuilder

--- src/AspNetCore/GraphQLExtensions.cs
+++ src/AspNetCore/GraphQLExtensions.cs
@@ -10,11 +10,31 @@ namespace GraphQL.Server.Transports.AspNetCore
 {
     public static class GraphQLExtensions
     {
+        /// <summary>
+        /// Adds the GraphQLHttp to services
+        /// </summary>
+        /// <param name="services">The servicecollection to registrer the services on</param>
+        /// <returns>The <see cref="IServiceCollection"/> recieved as parameter</returns>
         public static IServiceCollection AddGraphQLHttp(this IServiceCollection services)
         {
             services.TryAddSingleton<IDocumentWriter, DocumentWriter>();
             services.TryAddSingleton<IDocumentExecuter, DocumentExecuter>();
             services.TryAddSingleton<ISubscriptionExecuter, SubscriptionExecuter>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the GraphQLHttp to services
+        /// </summary>
+        /// <typeparam name="TUserContextBuilder">The <see cref="IUserContextBuilder"/> to use for generating the userContext used for the GraphQL request</typeparam>
+        /// <param name="services">The servicecollection to registrer the services on</param>
+        /// <returns>The <see cref="IServiceCollection"/> recieved as parameter</returns>
+        public static IServiceCollection AddGraphQLHttp<TUserContextBuilder>(this IServiceCollection services)
+            where TUserContextBuilder : class, IUserContextBuilder
+        {
+            services.AddGraphQLHttp();
+            services.TryAddTransient<IUserContextBuilder, TUserContextBuilder>();
 
             return services;
         }
