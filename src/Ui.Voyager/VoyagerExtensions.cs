80ec94e2ecd2e755f10f1c6c2a744527c59bcebc
Benjamin <bhu@askcody.dk>
2018-03-21 16:45:06 +0100

    Added Voyager middleware (#73)

---
+++ src/Ui.Voyager/VoyagerExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+
+namespace GraphQL.Server.Ui.Voyager
+{
+    public static class VoyagerExtensions
+    {
+        public static IApplicationBuilder UseGraphQLVoyager(this IApplicationBuilder app, GraphQLVoyagerOptions options)
+        {
+            if (options == null) { throw new ArgumentNullException(nameof(options)); }
+
+            app.UseMiddleware<VoyagerMiddleware>(options);
+            return app;
+        }
+    }
+}
