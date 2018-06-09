c69c6dcb15b3722dc40d6c908f944d96c5a6d656
Muhammad Rehan Saeed <RehanSaeed@users.noreply.github.com>
2018-06-09 07:21:16 +0100

    Initialize the DocumentWriter so it indents in development and uses the MVC built in JsonSerializerSettings. (#121)

--- src/Transports.AspNetCore/GraphQLExtensions.cs
+++ src/Transports.AspNetCore/GraphQLExtensions.cs
@@ -1,10 +1,11 @@
 using GraphQL.Http;
-using GraphQL.Subscription;
 using GraphQL.Types;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace GraphQL.Server.Transports.AspNetCore
 {
@@ -13,11 +14,19 @@ namespace GraphQL.Server.Transports.AspNetCore
         /// <summary>
         /// Adds the GraphQLHttp to services
         /// </summary>
-        /// <param name="services">The servicecollection to registrer the services on</param>
-        /// <returns>The <see cref="IServiceCollection"/> recieved as parameter</returns>
+        /// <param name="services">The service collection to register the services on</param>
+        /// <returns>The <see cref="IServiceCollection"/> received as parameter</returns>
         public static IServiceCollection AddGraphQLHttp(this IServiceCollection services)
         {
-            services.TryAddSingleton<IDocumentWriter, DocumentWriter>();
+            services.TryAddSingleton<IDocumentWriter>(
+                x =>
+                {
+                    var hostingEnvironment = x.GetRequiredService<IHostingEnvironment>();
+                    var jsonSerializerSettings = x.GetRequiredService<IOptions<JsonSerializerSettings>>();
+                    return new DocumentWriter(
+                        hostingEnvironment.IsDevelopment() ? Formatting.Indented : Formatting.None,
+                        jsonSerializerSettings.Value);
+                });
             services.TryAddSingleton<IDocumentExecuter, DocumentExecuter>();
 
             return services;
@@ -27,8 +36,8 @@ namespace GraphQL.Server.Transports.AspNetCore
         /// Adds the GraphQLHttp to services
         /// </summary>
         /// <typeparam name="TUserContextBuilder">The <see cref="IUserContextBuilder"/> to use for generating the userContext used for the GraphQL request</typeparam>
-        /// <param name="services">The servicecollection to registrer the services on</param>
-        /// <returns>The <see cref="IServiceCollection"/> recieved as parameter</returns>
+        /// <param name="services">The service collection to register the services on</param>
+        /// <returns>The <see cref="IServiceCollection"/> received as parameter</returns>
         public static IServiceCollection AddGraphQLHttp<TUserContextBuilder>(this IServiceCollection services)
             where TUserContextBuilder : class, IUserContextBuilder
         {
@@ -43,7 +52,7 @@ namespace GraphQL.Server.Transports.AspNetCore
         /// </summary>
         /// <typeparam name="TSchema">The implementation of <see cref="ISchema"/> to use</typeparam>
         /// <param name="builder">The <see cref="IApplicationBuilder"/> to use the middleware on</param>
-        /// <returns>The <see cref="IApplicationBuilder"/> recieved as parameter</returns>
+        /// <returns>The <see cref="IApplicationBuilder"/> received as parameter</returns>
         public static IApplicationBuilder UseGraphQLHttp<TSchema>(this IApplicationBuilder builder,
             GraphQLHttpOptions graphQLHttpOptions)
             where TSchema : ISchema
