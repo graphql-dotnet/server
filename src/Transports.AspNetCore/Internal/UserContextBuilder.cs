2a3ae05a1017c3eaa92651c6220b2f8fa18852b5
John Rutherford <sectrean@gmail.com>
2018-08-12 15:14:19 -0400

    Add extension methods for easily setting up an IUserContextBuilder

---
+++ src/Transports.AspNetCore/Internal/UserContextBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace GraphQL.Server.Transports.AspNetCore.Internal
+{
+    public class UserContextBuilder<TUserContext> : IUserContextBuilder
+    {
+        private readonly Func<HttpContext, Task<TUserContext>> _func;
+
+        public UserContextBuilder(Func<HttpContext, Task<TUserContext>> func)
+        {
+            _func = func ?? throw new ArgumentNullException(nameof(func));
+        }
+
+        public UserContextBuilder(Func<HttpContext, TUserContext> func)
+        {
+            if (func == null)
+                throw new ArgumentNullException(nameof(func));
+
+            _func = x => Task.FromResult(func(x));
+        }
+
+        public async Task<object> BuildUserContext(HttpContext httpContext)
+        {
+            return await _func(httpContext);
+        }
+    }
+}
