23f74642c40f6891ecdcd2d59de2769c0f276abd
Muhammad Rehan Saeed <RehanSaeed@users.noreply.github.com>
2018-08-15 17:03:49 +0100

    Add an overload to default the path to /graphql (#153)

--- src/Transports.AspNetCore/ApplicationBuilderExtensions.cs
+++ src/Transports.AspNetCore/ApplicationBuilderExtensions.cs
@@ -11,11 +11,23 @@ namespace GraphQL.Server
         /// Add the GraphQL middleware to the request pipeline
         /// </summary>
         /// <typeparam name="TSchema">The implementation of <see cref="ISchema"/> to use</typeparam>
-        /// <param name="builder"></param>
+        /// <param name="builder">The application builder.</param>
+        /// <param name="path">The path to the GraphQL endpoint which defaults to '/graphql'</param>
+        /// <returns>The <see cref="IApplicationBuilder"/> received as parameter</returns>
+        public static IApplicationBuilder UseGraphQL<TSchema>(this IApplicationBuilder builder, string path = "/graphql")
+            where TSchema : ISchema
+        {
+            return builder.UseGraphQL<TSchema>(new PathString(path));
+        }
+
+        /// <summary>
+        /// Add the GraphQL middleware to the request pipeline
+        /// </summary>
+        /// <typeparam name="TSchema">The implementation of <see cref="ISchema"/> to use</typeparam>
+        /// <param name="builder">The application builder.</param>
         /// <param name="path"></param>
         /// <returns>The <see cref="IApplicationBuilder"/> received as parameter</returns>
-        public static IApplicationBuilder UseGraphQL<TSchema>(this IApplicationBuilder builder,
-            PathString path)
+        public static IApplicationBuilder UseGraphQL<TSchema>(this IApplicationBuilder builder, PathString path)
             where TSchema : ISchema
         {
             return builder.UseMiddleware<GraphQLHttpMiddleware<TSchema>>(path);
--- src/Transports.Subscriptions.WebSockets/ApplicationBuilderExtensions.cs
+++ src/Transports.Subscriptions.WebSockets/ApplicationBuilderExtensions.cs
@@ -10,11 +10,27 @@ namespace GraphQL.Server
         /// <summary>
         /// Add GraphQL web sockets middleware to the request pipeline
         /// </summary>
-        /// <typeparam name="TSchema"></typeparam>
+        /// <typeparam name="TSchema">The implementation of <see cref="ISchema"/> to use</typeparam>
+        /// <param name="builder"></param>
+        /// <param name="path">The path to the GraphQL web socket endpoint which defaults to '/graphql'</param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseGraphQLWebSockets<TSchema>(
+            this IApplicationBuilder builder,
+            string path = "/graphql")
+            where TSchema : ISchema
+        {
+            return builder.UseGraphQLWebSockets<TSchema>(new PathString(path));
+        }
+
+        /// <summary>
+        /// Add GraphQL web sockets middleware to the request pipeline
+        /// </summary>
+        /// <typeparam name="TSchema">The implementation of <see cref="ISchema"/> to use</typeparam>
         /// <param name="builder"></param>
         /// <param name="path"></param>
         /// <returns></returns>
-        public static IApplicationBuilder UseGraphQLWebSockets<TSchema>(this IApplicationBuilder builder,
+        public static IApplicationBuilder UseGraphQLWebSockets<TSchema>(
+            this IApplicationBuilder builder,
             PathString path)
             where TSchema : ISchema
         {
