0d2fed6c9f7b2f1a49100f3d9fe63cd1b3e0b5dc
Muhammad Rehan Saeed <rehan.saeed@bridgeinternationalacademies.com>
2018-08-15 15:06:15 +0100

    Update comments

--- src/Transports.AspNetCore/GraphQLBuilderExtensions.cs
+++ src/Transports.AspNetCore/GraphQLBuilderExtensions.cs
@@ -10,12 +10,11 @@ namespace GraphQL.Server
     public static class GraphQLBuilderExtensions
     {
         /// <summary>
-        /// Set up a delegate to create the UserContext for each GraphQL request
+        /// Adds an <see cref="IUserContextBuilder"/> as a singleton.
         /// </summary>
-        /// <typeparam name="TUserContext"></typeparam>
-        /// <param name="builder"></param>
-        /// <param name="creator"></param>
-        /// <returns></returns>
+        /// <typeparam name="TUserContext">The type of the <see cref="IUserContextBuilder"/> implementation.</typeparam>
+        /// <param name="builder">The GraphQL builder.</param>
+        /// <returns>The GraphQL builder.</returns>
         public static IGraphQLBuilder AddUserContextBuilder<TUserContextBuilder>(this IGraphQLBuilder builder)
             where TUserContextBuilder : class, IUserContextBuilder
         {
@@ -28,9 +27,9 @@ namespace GraphQL.Server
         /// Set up a delegate to create the UserContext for each GraphQL request
         /// </summary>
         /// <typeparam name="TUserContext"></typeparam>
-        /// <param name="builder"></param>
-        /// <param name="creator"></param>
-        /// <returns></returns>
+        /// <param name="builder">The GraphQL builder.</param>
+        /// <param name="creator">A delegate used to create the user context from the <see cref="HttpContext"/>.</param>
+        /// <returns>The GraphQL builder.</returns>
         public static IGraphQLBuilder AddUserContextBuilder<TUserContext>(this IGraphQLBuilder builder, Func<HttpContext, TUserContext> creator)
             where TUserContext : class
         {
@@ -43,9 +42,9 @@ namespace GraphQL.Server
         /// Set up a delegate to create the UserContext for each GraphQL request
         /// </summary>
         /// <typeparam name="TUserContext"></typeparam>
-        /// <param name="builder"></param>
-        /// <param name="creator"></param>
-        /// <returns></returns>
+        /// <param name="builder">The GraphQL builder.</param>
+        /// <param name="creator">A delegate used to create the user context from the <see cref="HttpContext"/>.</param>
+        /// <returns>The GraphQL builder.</returns>
         public static IGraphQLBuilder AddUserContextBuilder<TUserContext>(this IGraphQLBuilder builder, Func<HttpContext, Task<TUserContext>> creator)
             where TUserContext : class
         {
