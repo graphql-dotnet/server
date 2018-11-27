a80df99e7c96d52205e3b428ec76be93c20608f2
Dzmitry Safarau <DSilencea@gmail.com>
2018-11-27 14:05:36 +0300

    Implemented unit tests for payloads of varying size

---
+++ tests/Transports.Subscriptions.WebSockets.Tests/TestMessage.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GraphQL.Server.Transports.WebSockets.Tests
+{
+    public class TestMessage
+    {
+        public string Content { get; set; }
+
+        public DateTimeOffset SentAt { get; set; }
+    }
+}
