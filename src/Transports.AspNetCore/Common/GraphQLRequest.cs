d26142f8eca80717e4481f5cb74c0281e473c0d0
Ben McCallum <ben.mccallum@live.com.au>
2020-01-06 19:30:52 +0100

    Custom property name as per Newtonsoft.Json (not working tho...)

--- src/Transports.AspNetCore/Common/GraphQLRequest.cs
+++ src/Transports.AspNetCore/Common/GraphQLRequest.cs
@@ -1,5 +1,10 @@
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+
+#if NETSTANDARD2_0
+using Newtonsoft.Json;
+#else
+using System.Text.Json.Serialization;
+#endif
 
 namespace GraphQL.Server.Transports.AspNetCore.Common
 {
@@ -9,13 +14,25 @@ namespace GraphQL.Server.Transports.AspNetCore.Common
         public const string VariablesKey = "variables";
         public const string OperationNameKey = "operationName";
 
+#if NETSTANDARD2_0
         [JsonProperty(QueryKey)]
+#else
+        [JsonPropertyName(QueryKey)]
+#endif
         public string Query { get; set; }
 
+#if NETSTANDARD2_0
         [JsonProperty(VariablesKey)]
+#else
+        [JsonPropertyName(VariablesKey)]
+#endif
         public JObject Variables { get; set; }
 
+#if NETSTANDARD2_0
         [JsonProperty(OperationNameKey)]
+#else
+        [JsonPropertyName(OperationNameKey)]
+#endif
         public string OperationName { get; set; }
 
         public Inputs GetInputs() => GetInputs(Variables);
