7dedb08110aa8c7185607072dad2b014d5c04963
Ben McCallum <ben.mccallum@live.com.au>
2020-01-21 11:11:20 +0100

    Working through feedback

--- src/Serialization.SystemTextJson/GraphQLRequestDeserializer.cs
+++ src/Serialization.SystemTextJson/GraphQLRequestDeserializer.cs
@@ -9,6 +9,17 @@ namespace GraphQL.Server.Serialization.SystemTextJson
 {
     public class GraphQLRequestDeserializer : IGraphQLRequestDeserializer
     {
+        /// <summary>
+        /// Max size of a buffer we can create using the Shared ArrayPool.
+        /// </summary>
+        /// <remarks>
+        /// Determined by following <see cref="ArrayPool{T}.Shared" /> (https://github.com/dotnet/coreclr/blob/master/src/System.Private.CoreLib/shared/System/Buffers/ArrayPool.cs)
+        /// which instantiates a TlsOverPerCoreLockedStacksArrayPool (https://github.com/dotnet/runtime/blob/ccf6aedb63c37ea8e10e4f5b5d9d23a69bdd9489/src/libraries/System.Private.CoreLib/src/System/Buffers/TlsOverPerCoreLockedStacksArrayPool.cs)
+        /// whose ctor calls Utilities.GetMaxBucketSize with TlsOverPerCoreLockedStacksArrayPool.NumBuckets = 17 (https://github.com/dotnet/runtime/blob/ccf6aedb63c37ea8e10e4f5b5d9d23a69bdd9489/src/libraries/System.Private.CoreLib/src/System/Buffers/Utilities.cs#L22)
+        /// which does 16 << 17 = 2MB as below.
+        /// </remarks>
+        private const int MaxSharedBufferSize = 2097152; 
+
         private readonly JsonSerializerOptions _serializerOptions = new JsonSerializerOptions();
 
         public GraphQLRequestDeserializer(Action<JsonSerializerOptions> configure)
@@ -18,51 +29,37 @@ namespace GraphQL.Server.Serialization.SystemTextJson
 
         public async Task<GraphQLRequestDeserializationResult> FromBodyAsync(Stream stream, long? contentLength)
         {
-            int length;
-            byte[] jsonBytes;
             var sharedArrayPool = ArrayPool<byte>.Shared;
-            bool wasRented = false;
-            if (contentLength.HasValue)
+            if (contentLength.HasValue && contentLength < MaxSharedBufferSize)
             {
-                length = (int)contentLength;
-                jsonBytes = sharedArrayPool.Rent(length);
-                wasRented = true;
+                var length = (int)contentLength.Value; // MaxSharedBufferSize < int.MaxValue so this is OK
+                var jsonBytes = sharedArrayPool.Rent(length); 
                 await stream.ReadAsync(jsonBytes, 0, length);
+
+                try
+                {
+                    return Process(jsonBytes.AsSpan(0, length));
+                }
+                finally
+                {
+                    sharedArrayPool.Return(jsonBytes);
+                }
             }
             else
             {
                 using var ms = new MemoryStream();
                 await stream.CopyToAsync(ms).ConfigureAwait(false);
-                if (!ms.TryGetBuffer(out var streamBytes))
-                {
-                    return new GraphQLRequestDeserializationResult();
-                }
-                jsonBytes = streamBytes.Array;
-                length = streamBytes.Count;
-            }
-
-            try
-            {
-                return Process(jsonBytes, length);
-            }
-            finally
-            {
-                if (wasRented)
-                {
-                    sharedArrayPool.Return(jsonBytes);
-                }
+                var jsonBytes = ms.GetBuffer();
+                return Process(jsonBytes.AsSpan(0, ms.Length));
             }
         }
 
-        private GraphQLRequestDeserializationResult Process(byte[] jsonBytes, int length)
+        private GraphQLRequestDeserializationResult Process(ReadOnlySpan<byte> jsonBytes)
         {
-            // Trim to the real length, as when using the shared array pool the buffer returned can be bigger than desired
-            var jsonData = jsonBytes.AsSpan(0, length);
-
             JsonTokenType tokenType;
             try
             {
-                tokenType = GetTokenType(jsonData);
+                tokenType = GetTokenType(jsonBytes);
             }
             catch (Exception)
             {
@@ -73,10 +70,10 @@ namespace GraphQL.Server.Serialization.SystemTextJson
             switch (tokenType)
             {
                 case JsonTokenType.StartObject:
-                    result.Single = JsonSerializer.Deserialize<GraphQLRequest>(jsonData, _serializerOptions);
+                    result.Single = JsonSerializer.Deserialize<GraphQLRequest>(jsonBytes, _serializerOptions);
                     break;
                 case JsonTokenType.StartArray:
-                    result.Batch = JsonSerializer.Deserialize<GraphQLRequest[]>(jsonData, _serializerOptions);
+                    result.Batch = JsonSerializer.Deserialize<GraphQLRequest[]>(jsonBytes, _serializerOptions);
                     break;
                 default:
                     result.WasSuccessful = false;
@@ -85,9 +82,9 @@ namespace GraphQL.Server.Serialization.SystemTextJson
             return result;
         }
 
-        private static JsonTokenType GetTokenType(Span<byte> jsonData)
+        private static JsonTokenType GetTokenType(ReadOnlySpan<byte> jsonBytes)
         {
-            var reader = new Utf8JsonReader(jsonData);
+            var reader = new Utf8JsonReader(jsonBytes);
             var success = reader.Read();
             return success ? reader.TokenType : JsonTokenType.None;
         }
