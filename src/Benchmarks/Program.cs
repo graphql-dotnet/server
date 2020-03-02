a85b6af7667f7d368171775e1102a696ce39b744
Ben McCallum <ben.mccallum@live.com.au>
2020-03-02 10:12:46 +0100

    More tweaks

--- src/Benchmarks/Program.cs
+++ src/Benchmarks/Program.cs
@@ -6,7 +6,7 @@ namespace GraphQL.Server.Benchmarks
     public class Program
     {
         // Call without args to just run the body deserializer benchmark
-        // Call with an int arg to toggle different tests
+        // Call with an int arg to toggle different benchmarks
         public static void Main(string[] args)
         {
             if (!args.Any() || !int.TryParse(args[0], out int benchmarkIndex))
