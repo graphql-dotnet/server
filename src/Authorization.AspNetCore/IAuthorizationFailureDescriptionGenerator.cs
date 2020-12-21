8b1c31d056e8355df6522c1a50922d33c1e7d90a
Alexander Rose <alex@rose-a.de>
2020-12-21 22:35:22 +0100

    Update src/Authorization.AspNetCore/IAuthorizationFailureDescriptionGenerator.cs
    Co-authored-by: Ivan Maximov <sungam3r@yandex.ru>

--- src/Authorization.AspNetCore/IAuthorizationFailureDescriptionGenerator.cs
+++ src/Authorization.AspNetCore/IAuthorizationFailureDescriptionGenerator.cs
@@ -12,7 +12,7 @@ namespace GraphQL.Server.Authorization.AspNetCore
         /// Generates a failure description for the given <paramref name="result"/> 
         /// </summary>
         /// <param name="result">The ASP.NET Core authorization result</param>
-        /// <param name="operationType">the GraphQL operation type</param>
+        /// <param name="operationType">The GraphQL operation type. Not set if authorization occurs for the schema itself</param>
         /// <returns></returns>
         string GenerateFailureDescription(AuthorizationResult result, OperationType? operationType);
     }
