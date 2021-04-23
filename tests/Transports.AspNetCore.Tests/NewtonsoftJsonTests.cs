94ce86fe469eabadf65c2ad4e4c993a551435a99
Shane32 <shane@acdmail.com>
2021-04-23 19:51:34 -0400

    Usings

--- tests/Transports.AspNetCore.Tests/NewtonsoftJsonTests.cs
+++ tests/Transports.AspNetCore.Tests/NewtonsoftJsonTests.cs
@@ -1,15 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
-using GraphQL.Execution;
-using GraphQL.NewtonsoftJson;
-using GraphQL.Server.Transports.Subscriptions.Abstractions;
-using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 using Shouldly;
 using Xunit;
 
