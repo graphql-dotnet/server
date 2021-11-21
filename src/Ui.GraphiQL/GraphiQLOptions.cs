fe2beee35457b87ba6bbf3c02148ad868edb6dd6
Sébastien Boulay <boulc@boulc.com>
2021-11-20 20:48:59 -0500

    feat: add additional options for GraphiQL

--- src/Ui.GraphiQL/GraphiQLOptions.cs
+++ src/Ui.GraphiQL/GraphiQLOptions.cs
@@ -22,5 +22,16 @@ namespace GraphQL.Server.Ui.GraphiQL
         /// HTTP headers with which the GraphiQL will be initialized.
         /// </summary>
         public Dictionary<string, string> Headers { get; set; }
+
+        /// <summary>
+        /// Enables the header editor when <c>true</c>.
+        /// Not supported when ExplorerExtensionEnabled is <c>true</c>.
+        /// </summary>
+        public bool HeaderEditorEnabled { get; set; } = true;
+
+        /// <summary>
+        /// Enables the explorer extension when <c>true</c>.
+        /// </summary>
+        public bool ExplorerExtensionEnabled { get; set; } = true;
     }
 }
