7d780f7d7f6c9c4f421a850d7248cdb30f2bbeac
Shane Krueger <shane@acdmail.com>
2021-12-27 14:25:05 -0500

    Mark old IGraphQLBuilder extension methods obsolete and create new ones (#602)

--- src/Transports.AspNetCore.NewtonsoftJson/GraphQLBuilderNewtonsoftJsonExtensions.cs
+++ src/Transports.AspNetCore.NewtonsoftJson/GraphQLBuilderNewtonsoftJsonExtensions.cs
@@ -15,7 +15,7 @@ namespace GraphQL.Server
         /// Adds a <see cref="IGraphQLRequestDeserializer"/> and a <see cref="IDocumentWriter"/>
         /// to the service collection with the provided configuration/settings.
         /// </summary>
-        /// <param name="builder"></param>
+        /// <param name="builder">The <see cref="IGraphQLBuilder"/>.</param>
         /// <param name="configureDeserializerSettings">
         /// Action to further configure the request deserializer's settings.
         /// Affects reading of the JSON from the HTTP request the middleware processes.
@@ -25,12 +25,37 @@ namespace GraphQL.Server
         /// Affects JSON returned by the middleware.
         /// </param>
         /// <returns>GraphQL Builder.</returns>
+        [Obsolete]
         public static IGraphQLBuilder AddNewtonsoftJson(this IGraphQLBuilder builder,
             Action<JsonSerializerSettings> configureDeserializerSettings = null,
             Action<JsonSerializerSettings> configureSerializerSettings = null)
         {
             builder.Services.AddSingleton<IGraphQLRequestDeserializer>(p => new GraphQLRequestDeserializer(configureDeserializerSettings ?? (_ => { })));
             builder.Services.Replace(ServiceDescriptor.Singleton<IDocumentWriter>(p => new DocumentWriter(configureSerializerSettings ?? (_ => { }), p.GetService<IErrorInfoProvider>() ?? new ErrorInfoProvider())));
+
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds a <see cref="IGraphQLRequestDeserializer"/> and a <see cref="IDocumentWriter"/>
+        /// to the service collection with the provided configuration/settings.
+        /// </summary>
+        /// <param name="builder">The <see cref="DI.IGraphQLBuilder"/>.</param>
+        /// <param name="configureDeserializerSettings">
+        /// Action to further configure the request deserializer's settings.
+        /// Affects reading of the JSON from the HTTP request the middleware processes.
+        /// </param>
+        /// <param name="configureSerializerSettings">
+        /// Action to further configure the response serializer's settings.
+        /// Affects JSON returned by the middleware.
+        /// </param>
+        /// <returns>GraphQL Builder.</returns>
+        public static DI.IGraphQLBuilder AddNewtonsoftJson(this DI.IGraphQLBuilder builder,
+            Action<JsonSerializerSettings> configureDeserializerSettings = null,
+            Action<JsonSerializerSettings> configureSerializerSettings = null)
+        {
+            builder.Register<IGraphQLRequestDeserializer>(p => new GraphQLRequestDeserializer(configureDeserializerSettings ?? (_ => { })), DI.ServiceLifetime.Singleton);
+            NewtonsoftJson.GraphQLBuilderExtensions.AddNewtonsoftJson(builder, configureSerializerSettings);
 
             return builder;
         }
