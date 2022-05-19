27741bf7663b7ac601d6f2e81c0bbda7f9c91bff
Shane Krueger <Shane@acdmail.com>
2022-05-19 16:28:45 -0400

    Update

--- src/Transports.AspNetCore/AuthorizationParameters.cs
+++ src/Transports.AspNetCore/AuthorizationParameters.cs
@@ -8,8 +8,10 @@ using Microsoft.AspNetCore.Http;
 namespace GraphQL.Server.Transports.AspNetCore;
 
 /// <summary>
-/// Authorization parameters. This struct is used to group all necessary parameters together and perform arbitrary
-/// actions based on provided authentication properties/attributes/etc. It is not intended to be called from user code.
+/// Authorization parameters.
+/// This struct is used to group all necessary parameters together and perform arbitrary
+/// actions based on provided authentication properties/attributes/etc.
+/// It is not intended to be called from user code.
 /// </summary>
 public struct AuthorizationParameters<TState>
 {
