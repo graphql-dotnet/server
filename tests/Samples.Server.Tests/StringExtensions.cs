06eaeeaedddf603a53a82f7bccb43855ff5fe1d7
Shane32 <shane@acdmail.com>
2022-06-23 12:05:16 -0400

    Restore Samples.Server.Tests

---
+++ tests/Samples.Server.Tests/StringExtensions.cs
@@ -0,0 +1,9 @@
+using GraphQL;
+
+namespace Samples.Server.Tests;
+
+internal static class StringExtensions
+{
+    public static Inputs? ToInputs(this string? value)
+        => new GraphQL.SystemTextJson.GraphQLSerializer().Deserialize<Inputs>(value);
+}
