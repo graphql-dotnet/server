2b447a83f73556e50bf1686b5c0b8064f00037bc
Shane Krueger <shane@acdmail.com>
2022-07-01 12:59:26 -0400

    Add ExecutionResultActionResult (#843)

---
+++ src/Transports.AspNetCore/ExecutionResultActionResult.cs
@@ -0,0 +1,37 @@
+namespace GraphQL.Server.Transports.AspNetCore;
+
+/// <summary>
+/// An action result that formats the <see cref="ExecutionResult"/> as JSON.
+/// </summary>
+public sealed class ExecutionResultActionResult : IActionResult
+{
+    private readonly ExecutionResult _executionResult;
+    private readonly HttpStatusCode _statusCode;
+
+    /// <inheritdoc cref="ExecutionResultActionResult"/>
+    public ExecutionResultActionResult(ExecutionResult executionResult)
+    {
+        _executionResult = executionResult;
+        _statusCode = executionResult.Executed ? HttpStatusCode.OK : HttpStatusCode.BadRequest;
+    }
+
+    /// <inheritdoc cref="ExecutionResultActionResult"/>
+    public ExecutionResultActionResult(ExecutionResult executionResult, HttpStatusCode statusCode)
+    {
+        _executionResult = executionResult;
+        _statusCode = statusCode;
+    }
+
+    /// <inheritdoc cref="HttpResponse.ContentType"/>
+    public string ContentType { get; set; } = GraphQLHttpMiddleware.CONTENTTYPE_GRAPHQLJSON;
+
+    /// <inheritdoc/>
+    public async Task ExecuteResultAsync(ActionContext context)
+    {
+        var serializer = context.HttpContext.RequestServices.GetRequiredService<IGraphQLSerializer>();
+        var response = context.HttpContext.Response;
+        response.ContentType = ContentType;
+        response.StatusCode = (int)_statusCode;
+        await serializer.WriteAsync(response.Body, _executionResult, context.HttpContext.RequestAborted);
+    }
+}
