b19b9029f254d781de17cedfcbb1e9073eb6ade6
Shane32 <shane@acdmail.com>
2022-07-04 23:45:07 -0400

    Update

--- tests/Samples.MultipleSchemas.Tests/EndToEndTests.cs
+++ tests/Samples.MultipleSchemas.Tests/EndToEndTests.cs
@@ -4,6 +4,11 @@ namespace Samples.MultipleSchemas.Tests;
 
 public class EndToEndTests
 {
+    private const string CAT_QUERY = @"{ cat(id:""1"") { name } }";
+    private const string CAT_RESPONSE = @"{""data"":{""cat"":{""name"":""Fluffy""}}}";
+    private const string DOG_QUERY = @"{ dog(id:""1"") { name } }";
+    private const string DOG_RESPONSE = @"{""data"":{""dog"":{""name"":""Shadow""}}}";
+
     [Fact]
     public Task Cats_Playground()
         => new ServerTests<Program>().VerifyPlaygroundAsync("/cats");
@@ -14,17 +19,25 @@ public class EndToEndTests
 
     [Fact]
     public Task Cats_GraphQLGet()
-        => new ServerTests<Program>().VerifyGraphQLGetAsync("/cats/graphql", @"{ cat(id:""1"") { name } }", @"{""data"":{""cat"":{""name"":""Fluffy""}}}");
+        => new ServerTests<Program>().VerifyGraphQLGetAsync("/cats/graphql", CAT_QUERY, CAT_RESPONSE);
 
     [Fact]
     public Task Cats_GraphQLPost()
-        => new ServerTests<Program>().VerifyGraphQLPostAsync("/cats/graphql", @"{ cat(id:""1"") { name } }", @"{""data"":{""cat"":{""name"":""Fluffy""}}}");
+        => new ServerTests<Program>().VerifyGraphQLPostAsync("/cats/graphql", CAT_QUERY, CAT_RESPONSE);
+
+    [Fact]
+    public Task Cats_GraphQLWebSockets()
+        => new ServerTests<Program>().VerifyGraphQLWebSocketsAsync("/cats/graphql", CAT_QUERY, CAT_RESPONSE);
 
     [Fact]
     public Task Dogs_GraphQLGet()
-        => new ServerTests<Program>().VerifyGraphQLGetAsync("/dogs/graphql", @"{ dog(id:""1"") { name } }", @"{""data"":{""dog"":{""name"":""Shadow""}}}");
+        => new ServerTests<Program>().VerifyGraphQLGetAsync("/dogs/graphql", DOG_QUERY, DOG_RESPONSE);
 
     [Fact]
     public Task Dogs_GraphQLPost()
-        => new ServerTests<Program>().VerifyGraphQLPostAsync("/dogs/graphql", @"{ dog(id:""1"") { name } }", @"{""data"":{""dog"":{""name"":""Shadow""}}}");
+        => new ServerTests<Program>().VerifyGraphQLPostAsync("/dogs/graphql", DOG_QUERY, DOG_RESPONSE);
+
+    [Fact]
+    public Task Dogs_GraphQLWebSockets()
+        => new ServerTests<Program>().VerifyGraphQLWebSocketsAsync("/dogs/graphql", DOG_QUERY, DOG_RESPONSE);
 }
