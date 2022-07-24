9cf5d644499375bcdb193eb5deb632285bf435de
thorstenbaek <tst@dips.no>
2022-07-24 19:03:09 +0200

    Removed private variable storing built HTML

--- src/Ui.SmartPlayground/Internal/SmartPlaygroundPageModel.cs
+++ src/Ui.SmartPlayground/Internal/SmartPlaygroundPageModel.cs
@@ -6,8 +6,6 @@ namespace GraphQL.Server.Ui.SmartPlayground.Internal;
 // https://docs.microsoft.com/en-us/aspnet/core/mvc/razor-pages/?tabs=netcore-cli
 internal sealed class PlaygroundPageModel
 {
-    private string? _playgroundCSHtml;
-
     private readonly SmartPlaygroundOptions _options;
 
     public PlaygroundPageModel(SmartPlaygroundOptions options)
@@ -17,43 +15,38 @@ internal sealed class PlaygroundPageModel
 
     public string Render()
     {
-        if (_playgroundCSHtml == null)
+        using var manifestResourceStream = _options.IndexStream(_options);
+        using var streamReader = new StreamReader(manifestResourceStream);
+
+        var headers = new Dictionary<string, object>
         {
-            using var manifestResourceStream = _options.IndexStream(_options);
-            using var streamReader = new StreamReader(manifestResourceStream);
+            ["Accept"] = "application/json",
+            // TODO: investigate, fails in Chrome
+            // {
+            //   "error": "Response not successful: Received status code 400"
+            // }
+            //
+            // MediaTypeHeaderValue.TryParse(httpRequest.ContentType, out var mediaTypeHeader) from GraphQLHttpMiddleware
+            // returns false because of
+            // content-type: application/json, application/json
 
-            var headers = new Dictionary<string, object>
-            {
-                ["Accept"] = "application/json",
-                // TODO: investigate, fails in Chrome
-                // {
-                //   "error": "Response not successful: Received status code 400"
-                // }
-                //
-                // MediaTypeHeaderValue.TryParse(httpRequest.ContentType, out var mediaTypeHeader) from GraphQLHttpMiddleware
-                // returns false because of
-                // content-type: application/json, application/json
+            //["Content-Type"] = "application/json",
+        };
 
-                //["Content-Type"] = "application/json",
-            };
-
-            if (_options.Headers?.Count > 0)
-            {
-                foreach (var item in _options.Headers)
-                    headers[item.Key] = item.Value;
-            }
-
-            var builder = new StringBuilder(streamReader.ReadToEnd())
-
-                .Replace("@Model.GraphQLEndPoint", _options.GraphQLEndPoint.ToString())
-                .Replace("@Model.SubscriptionsEndPoint", _options.SubscriptionsEndPoint.ToString())
-                .Replace("@Model.GraphQLConfig", JsonSerializer.Serialize<object>(_options.GraphQLConfig!))
-                .Replace("@Model.Headers", JsonSerializer.Serialize<object>(headers))
-                .Replace("@Model.PlaygroundSettings", JsonSerializer.Serialize<object>(_options.PlaygroundSettings));
-
-            _playgroundCSHtml = _options.PostConfigure(_options, builder.ToString());
+        if (_options.Headers?.Count > 0)
+        {
+            foreach (var item in _options.Headers)
+                headers[item.Key] = item.Value;
         }
 
-        return _playgroundCSHtml;
+        var builder = new StringBuilder(streamReader.ReadToEnd())
+
+            .Replace("@Model.GraphQLEndPoint", _options.GraphQLEndPoint.ToString())
+            .Replace("@Model.SubscriptionsEndPoint", _options.SubscriptionsEndPoint.ToString())
+            .Replace("@Model.GraphQLConfig", JsonSerializer.Serialize<object>(_options.GraphQLConfig!))
+            .Replace("@Model.Headers", JsonSerializer.Serialize<object>(headers))
+            .Replace("@Model.PlaygroundSettings", JsonSerializer.Serialize<object>(_options.PlaygroundSettings));
+
+        return _options.PostConfigure(_options, builder.ToString());
     }
 }
