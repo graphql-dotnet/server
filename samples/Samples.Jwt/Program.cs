964cee3743c7d5532cbe79eaae0541fe234ba9f7
Shane Krueger <Shane@acdmail.com>
2022-08-02 02:04:26 -0400

    Update comments

--- samples/Samples.Jwt/Program.cs
+++ samples/Samples.Jwt/Program.cs
@@ -6,6 +6,7 @@ using Chat = GraphQL.Samples.Schemas.Chat;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+builder.Services.AddControllers();
 builder.Services.AddSingleton<Chat.IChat, Chat.Chat>();
 builder.Services.AddGraphQL(b => b
     .AddAutoSchema<Chat.Query>(s => s
@@ -14,36 +15,44 @@ builder.Services.AddGraphQL(b => b
     .AddSystemTextJson()
     // support authorization policies within the schema (although none are set in this sample)
     .AddAuthorizationRule()
-    // support WebSocket authentication via the payload of the initialization packet
+    // support WebSocket authentication via the payload of the initialization message
     .AddWebSocketAuthentication<JwtWebSocketAuthenticationService>());
 
-// provide authentication for GET/POST requests via the 'Authorization' HTTP header;
+// configure authentication for GET/POST requests via the 'Authorization' HTTP header;
 // will authenticate WebSocket requests as well, but browsers cannot set the
 // 'Authorization' HTTP header for WebSocket requests
 builder.Services.AddAuthentication(
     opts =>
     {
         opts.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-        // configure custom authorization policies here
+        // configure custom authorization policies here, if any
     })
     .AddJwtBearer(opts => opts.TokenValidationParameters = JwtHelper.TokenValidationParameters);
 
 var app = builder.Build();
 app.UseDeveloperExceptionPage();
+// enable WebSocket support within the ASP.NET Core framework
 app.UseWebSockets();
 // use ASP.Net Core authentication (again, for GET/POST requests mainly)
 app.UseAuthentication();
 // configure the graphql endpoint at "/graphql" for administrators only
 app.UseGraphQL("/graphql", opts =>
 {
-    // set a transport-level authorization policy; connections are refused if this policy is not met
-    // this means that anonymous access to the introspection query is not available
+    // here we are setting a transport-level authorization policy allowing only authenticated users
+    // that carry the "Administrator" role to connect; connections are refused if this policy is not met
+
+    // because of this rule, anonymous access to perform introspection queries is not available
+
+    // remove this authorization policy if you wish to allow anonymous access to your schema and set
+    // authorization policies on individual graphs and fields
+
     opts.AuthorizedRoles.Add("Administrator");
 });
+// enable ASP.Net Core routing for razor pages and MVC controllers
 app.UseRouting();
 // enable Pages for the GraphiQL demonstration endpoint at /
 app.MapRazorPages();
-// the OAuth2 authorization endpoint will be at /token
+// enable MVC controllers for the OAuth2 authorization endpoint at /token
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action}");
