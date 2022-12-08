6935c78c3325d0d7bb62bea1866e71a088dc022c
Shane Krueger <shane@acdmail.com>
2022-12-08 11:30:44 -0500

    Update samples/Samples.AzureFunctions/GraphQL.cs
    Co-authored-by: Ivan Maximov <sungam3r@yandex.ru>

--- samples/Samples.AzureFunctions/GraphQL.cs
+++ samples/Samples.AzureFunctions/GraphQL.cs
@@ -29,6 +29,6 @@ public class GraphQL
     {
         log.LogInformation("C# HTTP trigger function processed a request for the GraphQL Playground UI.");
 
-        return new PlaygroundActionResult(opts => opts.GraphQLEndPoint = "/api/graphql");
+        return new PlaygroundActionResult(opts => opts.GraphQLEndPoint = "/api/graphql"); // /api/graphql route will call RunGraphQL method
     }
 }
