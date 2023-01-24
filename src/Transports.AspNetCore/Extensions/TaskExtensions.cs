088ca44ae850ca4d4733b66908f4842c4b4881c7
Shane Krueger <shane@acdmail.com>
2023-01-24 11:44:25 -0500

    Update src/Transports.AspNetCore/Extensions/TaskExtensions.cs
    Co-authored-by: Ivan Maximov <sungam3r@yandex.ru>

--- src/Transports.AspNetCore/Extensions/TaskExtensions.cs
+++ src/Transports.AspNetCore/Extensions/TaskExtensions.cs
@@ -28,7 +28,6 @@ internal static class TaskExtensions
             // the 'using' here ensures that the Task.Delay gets disposed if the task finishes before the delay
             using (var timeoutCancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken))
             {
-
                 var completedTask = await Task.WhenAny(task, Task.Delay(millisecondsDelay, timeoutCancellationTokenSource.Token)).ConfigureAwait(false);
                 if (completedTask == task)
                 {
